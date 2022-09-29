d43497179e0f8350b1c6925ab917fdf865dc439b
Korovin Daniil <starwa87@gmail.com>
2022-09-29 21:31:04 +0300

    Labaratornaya 1

---
+++ MethLab1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MethLab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+        }
+        
+        public enum Education
+        {
+            Specialist,
+            Bachelor,
+            SecondEducation
+        }
+       
+    }
+}
